b878facbdd37f0a7343dcd27e9861a6d5c257b12
liwei <liwei@gfax.cn>
2018-07-05 15:15:45 +0800

    working for test node and add transfer money function

--- chapter3/app/javascripts/app.js
+++ chapter3/app/javascripts/app.js
@@ -22,7 +22,7 @@ var Voting = contract(voting_artifacts);
 //var contractid = '0x7b2ed2d8c914c55250e8b03276415a2b1dc44d0a';
 var contractid='0xA3b7045Df02C0745Edd62180dA46e2b2BCD8807f';
 let candidates = {}
-
+const nodefortest="http://i.mailwalk.com:8545";
 let tokenPrice = null;
 
 window.voteForCandidate = function(candidate) {
@@ -166,12 +166,12 @@ $( document ).ready(function() {
     // Use Mist/MetaMask's provider
 //    window.web3 = new Web3(web3.currentProvider);
 //    window.web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.10:8545"));
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.0.174:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider(nodefortest));
   } else {
     console.warn("No web3 detected. Falling back to http://localhost:8545. You should remove this fallback when you deploy live, as it's inherently insecure. Consider switching to Metamask for development. More info here: http://truffleframework.com/tutorials/truffle-and-metamask");
     // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
 //    window.web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.1.10:8545"));
-    window.web3 = new Web3(new Web3.providers.HttpProvider("http://192.168.0.174:8545"));
+    window.web3 = new Web3(new Web3.providers.HttpProvider(nodefortest));
   }
 
   Voting.setProvider(web3.currentProvider);
